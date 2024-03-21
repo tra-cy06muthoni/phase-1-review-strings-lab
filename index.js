6125c0fd4bc949a4babb2837f1d7c204c3e69955
tracy <tracy.muthoni@student.moringaschool.com>
2024-03-21 15:14:46 +0300

    addingmyfilemd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser='Tracy Muthoni '
+ 
+let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage='WELCOME TO FLATBOOK, ' +`${currentUser}`.toUpperCase() + '!';
+
+let shortGreeting='Welcome,'${currentUser}+`(T) +'!';
+
+'Edsger Dijkstra'[0];
+// =>"E"
